22d8624354b642882bc85ec42f3138c807a71d09
peishiyong <pyong@zendesk.com>
2022-08-11 16:07:13 +1000

    parsing from command line

--- Main.java
+++ Main.java
@@ -7,28 +7,26 @@ public class Main {
     Table table = new Table(5, 5);
     Robot robot = new Robot(table);
 
-    List<Command> commands = new ArrayList<>();
-    commands.add(new Command.Place(2, 2, Robot.Direction.NORTH));
-    commands.add(new Command.Move());
-    commands.add(new Command.Left());
-    commands.add(new Command.Move());
-    commands.add(new Command.Right());
-    commands.add(new Command.Report());
+    // List<Command> commands = new ArrayList<>();
+    // commands.add(new Command.Place(2, 2, Robot.Direction.NORTH));
+    // commands.add(new Command.Move());
+    // commands.add(new Command.Left());
+    // commands.add(new Command.Move());
+    // commands.add(new Command.Right());
+    // commands.add(new Command.Report());
 
-    for (Command command : commands) {
+    // for (Command command : commands) {
+    //   command.execute(robot);
+    // }
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (scanner.hasNextLine()) {
+      String str = scanner.nextLine();
+      Command command = Parser.accept(str);
       command.execute(robot);
     }
 
-    // Scanner scanner = new Scanner(System.in);
-
-    // while (scanner.hasNextLine()) {
-    // String str = scanner.nextLine();
-    // System.out.println(str);
-
-    // Command command = Parser.accept(str, table);
-    // command.execute(robot);
-    // }
-
-    // scanner.close();
+    scanner.close();
   }
 }
--- Parser.java
+++ Parser.java
@@ -1,7 +1,54 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Parser {
+  public static Command accept(String rawCommand) {
+    List<String> tokens = Arrays.asList(rawCommand.split(" "));
 
-  public static Command accept(String rawCommand, Table table) {
+    if (!tokens.isEmpty()) {
+      String part1 = tokens.get(0).toUpperCase();
+
+      switch (part1) {
+        case "PLACE":
+          return placeCommand(tokens);
+        case "LEFT":
+          return new Command.Left();
+        case "RIGHT":
+          return new Command.Right();
+        case "MOVE":
+          return new Command.Move();
+        case "REPORT":
+          return new Command.Report();
+        default:
+          //todo report error
+          return null;
+      }
+    }
+
     return null;
+  }
 
+  private static Command placeCommand(List<String> tokens) {
+    if(tokens.size() != 2) {
+      return null;
+    }
+
+    List<String> locationMeta = Arrays.asList(tokens.get(1).split(","));
+
+    if(locationMeta.size() != 3) {
+      return null; //todo report error
+    }
+
+    try {
+      int x = Integer.valueOf(locationMeta.get(0));
+      int y = Integer.valueOf(locationMeta.get(1));
+
+      Robot.Direction dir = Robot.Direction.valueOf(locationMeta.get(2));
+      return new Command.Place(x, y, dir);
+    } catch(NumberFormatException e) {
+      return null; //todo report error
+    } catch(IllegalArgumentException e) {
+      return null; //todo report error
+    }
   }
 }
